6a0a1b3b15eaf1c31f8c6a52ac20cd4579b03dfd
Strikewind <mhaariskhan1@gmail.com>
2022-05-23 15:51:24 +0100

    initial map sim 2

--- map_sim.py
+++ map_sim.py
@@ -7,18 +7,48 @@ from matplotlib.figure import Figure
 arena_x = 360
 arena_y = 240
 
+running = True
+
 mpl.rcParams['toolbar'] = 'None'
+plt.ion()
 
-def full_map ():
-	full_map = Figure(figsize=(7.2, 10.8))
-	full_map.canvas.set_window_title('Simulated Map')
-	plt.ylim([0, 240])
-	plt.xlim([0, 360])
-	plt.style.use('Solarize_Light2')
-	plt.grid(color='lightgray', lw=0.5)
-	dot = plt.scatter(0, 0, color='firebrick') ########
-	plt.show()
+class Map ():
 
-def random_test ():
+	def __init__ (self):
+		global map_fig
+		map_fig = Figure(figsize=(7.2, 10.8))
+		plt.style.use('Solarize_Light2')
+		plt.title('Simulated Map')
+		plt.ylim([0, 240])
+		plt.xlim([0, 360])
+		plt.grid(color='lightgray', lw=0.5)
+		self.dot = plt.scatter(0, 0, color='firebrick') ########
+		plt.show()
 
-full_map()
+def clear ():
+	try:
+		plt.close()
+	except:
+		pass
+	try:
+		map.__del__(self)
+		map_fig.__del__(self)
+	except:
+		pass
+
+def plot ():
+	global sim_map
+	sim_map.dot.set_offsets([np.random.randint(1, 360), np.random.randint(1, 240)]) #####
+	plt.pause(0.001)
+
+def run ():
+	global sim_map
+	clear()
+	sim_map = Map()
+	while running:
+		##subroutines
+		plot()
+
+
+if __name__ == "__main__":
+	run()
