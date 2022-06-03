b945e76230b24403a481ba089f76328ce61de1cc
bet20ICL <bet20@ic.ac.uk>
2022-06-03 11:15:39 +0100

    python - node through sockets

---
+++ webapp/server/echo_sockets.py
@@ -0,0 +1,10 @@
+import socketio
+
+sio = socketio.Client()
+sio.connect('http://localhost:3000')
+
+
+@sio.on('ntop')
+def on_message(data):
+    print(f'{data}')
+    sio.emit('pton', 'received')
--- webapp/server/server.js
+++ webapp/server/server.js
@@ -9,20 +9,20 @@ app.use(express.static(`${__dirname}/../client`));
 const socketio = require('socket.io');
 const io = socketio(server); 
 
-// method 1
-const {PythonShell} = require('python-shell');
-let pyshell = new PythonShell('echo_text.py', { mode: 'text' });
-// let output = "";
-pyshell.stdout.on('data', (data) => {
-    // output += '' + data;
-    console.log(`Python out: ${data}`);
-});
+// // method 1
+// const {PythonShell} = require('python-shell');
+// let pyshell = new PythonShell('echo_text.py', { mode: 'text' });
+// // let output = "";
+// pyshell.stdout.on('data', (data) => {
+//     // output += '' + data;
+//     console.log(`Python out: ${data}`);
+// });
 
 // pyshell.send('hello').send('world').end(function (err) {
 //     console.log(output === ('hello' + '\n' + 'world' + '\n'));
 //     console.log(output);
 // });
-pyshell.on('close', () => console.log("Process ended"));
+// pyshell.on('close', () => console.log("Process ended"));
 
 // // method 2
 // const {spawn} = require('child_process');
@@ -40,12 +40,18 @@ io.on('connection', (sock) => {
     sock.on('waypoint', ({ x, y }) => {
         io.emit('waypoint', { x, y });
         console.log(`waypoint placed at x: ${x}, y: ${y}`);
-        pyshell.send(`W,${x},${y}`);
+        io.emit('ntop', { x, y });
+        // pyshell.send(`W,${x},${y}`);
     });
     
     sock.on('stickmove', ({d, a}) => {
         console.log(`joystick at distance: ${d}, angle: ${a}`)
-        pyshell.send(`J,${d},${a}`);
+        // pyshell.send(`J,${d},${a}`);
+        io.emit('ntop', {x, y});
+    });
+
+    sock.on('pton', (s) => {
+        console.log(`Python out: ${s}`);
     });
 });
 
