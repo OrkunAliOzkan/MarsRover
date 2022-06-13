7730a4e2c55ed51831e82531b6b833ffdc274b68
Orkun Ozkan <43971772+OrkunAliOzkan@users.noreply.github.com>
2022-06-13 20:48:22 +0100

    Doesn't work :/

---
+++ DriveCommandControlMerge/.pio/build/esp32dev/libc43/SPI/SPI.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/libc43/SPI/SPI.cpp.o: \
+ C:/Users/Orkun/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.cpp \
+ C:/Users/Orkun/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.h \
+ C:/Users/Orkun/.platformio/packages/framework-arduinoespressif32/variants/esp32/pins_arduino.h \
+ C:/Users/Orkun/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-spi.h
