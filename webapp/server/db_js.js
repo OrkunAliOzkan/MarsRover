2afa3e4da5a8d1cbf844dc11f6fca7bb66a3b314
bet20ICL <bet20@ic.ac.uk>
2022-06-13 15:35:36 +0100

    tcp_server

--- webapp/server/db_js.js
+++ webapp/server/db_js.js
@@ -1,17 +1,56 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://bet20:TCuh4ZxixcbXFQS0@cluster0.agmw9dr.mongodb.net/?retryWrites=true&w=majority";
+const mongo = require('mongodb');
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Connected to MongoDB Atlas database");
-  // var mars_db = db.db("missions");
-  // mars_db.collection("mission_data").findOne({}, (err, result) => {
-  //   if (err) throw err;
-  //   console.log(result.name);
-  //   db.close();
-  // })
-  // mars_db.close();
-});
+(async function main() {
+    var url = "mongodb+srv://bet20:TCuh4ZxixcbXFQS0@cluster0.agmw9dr.mongodb.net/?retryWrites=true&w=majority";
+    const client = new mongo.MongoClient(url);
+    try {
+        await client.connect();
+        console.log("Connected to cluster");
+
+        await listDatabases(client);
+
+        const mars_db = client.db("missions");
+        
+        await printCollections(mars_db);
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
+})();
+
+async function printDocs(col) {
+    document_list = await 
+}
+
+async function printCollections(db) {
+    collection_list = await db.listCollections().toArray();
+    console.log(collection_list);
+    console.log("Collections:");
+    collection_list.forEach(collection => console.log(` - ${collection.name}`));
+}
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     console.log("Connected to MongoDB Atlas database");
+//     var mars_db = db.db("missions");
+//     // mars_db.collection("mission_data").findOne({}, (err, result) => {
+//     //   if (err) throw err;
+//     //   console.log(result.name);
+//     //   db.close();
+//     // })
+//     // mars_db.close();
+//     mars_db.collectionNames( (err, collections) => {
+//         console.log(collections);
+//     })
+// });
 
 const now = new Date("2022-06-10 23:16:39.603783+00:00");
 console.log(now);
---
+++ webapp/server/tcp_server.py
@@ -0,0 +1,23 @@
+import socket
+ 
+s = socket.socket()         
+ 
+s.bind(('0.0.0.0', 8090 ))
+s.listen(0)                 
+
+print("Server started on port 8090")
+while True:
+ 
+    client, addr = s.accept()
+ 
+    while True:
+        content = client.recv(32)
+ 
+        if len(content) ==0:
+           break
+ 
+        else:
+            print(content)
+ 
+    print("Closing connection")
+    client.close()
