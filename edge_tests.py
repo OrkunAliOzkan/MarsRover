278ea76189b20cf8df47d8a802ecb4611eae6721
Strikewind <mhaariskhan1@gmail.com>
2022-06-15 00:46:15 +0100

    edges1

--- edge_tests.py
+++ edge_tests.py
@@ -4,20 +4,24 @@ from PIL import Image
 
 start = time.time()
 
-img = Image.open('grating.jpeg')
+img = Image.open('inputs/edge_test.png')
 pixels = np.array(img)
 
+n = 3
 sobel_xx = [1, 0, -1]
+#sobel_xx = [2, 1, 0, -1, -2]
+#sobel_xx = [1, 1, 1, 0, -1, -1, -1]
+#sobel_xx = [1, 1, 1, 1, 1, 1, 0, -1, -1, -1, -1, -1, -1]
 # sobel_xy = [1, 2, 1]
 
-edge_buff = [0 for i in range(3)]
+edge_buff = [0 for i in range(n)]
 
 # horiz_edge_buff = [0 for i in range(1281)]	# rows + 1 pixel
 
 final = np.zeros(shape=(480, 640, 3))	#after all operations
 
 edge_x = 0
-# edge_y = 0
+#edge_y = 0
 
 
 def pre_assignments ():
@@ -28,38 +32,33 @@ def pre_assignments ():
 	grey = green/2 + red/4 + blue/4
 
 def input_buffer ():
-	if (x % 2 == 0): #lower resolution
-		for i in range(2, 0, -1):
-			edge_buff[i] = edge_buff[i-1]	#move items in delay line
-		edge_buff[0] = grey		#new values come in
-
-# def horiz_buffer ():
-# 	for i in range(1280, 0, -1):
-# 		horiz_edge_buff[i] = horiz_edge_buff[i-1]	#move rows in delay line
-# 	horiz_edge_buff[0] = edge_x		#writes to newest row of horiz
+	for i in range(n-1, 0, -1):
+		edge_buff[i] = edge_buff[i-1]	#move items in delay line
+	edge_buff[0] = grey		#new values come in
 
 # count_horiz = 0
 # def horiz_buffer ():
 # 	global count_horiz
-# 	count_horiz = (count_horiz + 1) % 1281
+# 	count_horiz = (count_horiz + 1) % (640*(n-1)+1)
 # 	horiz_edge_buff[count_horiz] = edge_x
 
 def edge_horiz ():
 	global edge_x
 	edge_x = 0
-	for i in range(0, 3, 1):
+	for i in range(0, n, 1):
 		edge_x = edge_x + sobel_xx[i]*edge_buff[i]
-	edge_x = np.sqrt(edge_x**2)
+	edge_x = edge_x / ((n-1)/2)
+	edge_x = abs(edge_x)
 	#edge_x = 255 if edge_x > 100 else 0
 
 # def edge_vert ():
 # 	global edge_y
 # 	edge_y = 0
-# 	for i in range(0, 3, 1):
-# 		edge_y = edge_y + sobel_xy[i]*horiz_edge_buff[(count_horiz+(640*i)) % 1281]
-# 	edge_y = edge_y / 2
+# 	for i in range(0, n, 1):
+# 		edge_y = edge_y + sobel_xy[i]*horiz_edge_buff[(count_horiz+(640*i)) % (640*(n-1)+1)]
+# 	edge_y = edge_y / ((n+2)/2)
 # 	edge_y = np.sqrt(edge_y**2)
-	#edge_y = 255 if edge_y > 70 else 0
+# 	edge_y = 255 if edge_y > 70 else 0
 
 def post_assignments ():
 	final[y, x, 0] = edge_x	#writes to current row of final
@@ -86,6 +85,6 @@ for i in range(480):
 			post_assignments()
 
 out = Image.fromarray(np.asarray(final).astype(np.uint8))
-out.save("out_edge.png")
+out.save("results/out_edge.png")
 
 print(f"Elapsed: {time.time() - start}")
--- processing.sv
+++ processing.sv
@@ -3,153 +3,43 @@ module processing (
 	input wire [7:0] red,
 	input wire [7:0] green,
 	input wire [7:0] blue,
-	input wire [23:0] grey,
+	input wire [7:0] grey,
 	input wire sop,
 	input wire packet_video,
 	input wire in_valid,
 	input wire x,
 	input wire y,
-
 	output wire [7:0] red_processed,
 	output wire [7:0] green_processed,
 	output wire [7:0] blue_processed
-
 );
 
-wire [15:0] Hue; //0-360.. % 360
-wire [7:0] Sat, Val; //0-100, 0-255
-wire [7:0] RGB_max, RGB_min;
-wire [7:0] RGB_diff;
+reg signed [23:0] sobel_x = {8'd1, 8'd0, -8'd1};   //[1, 0, -1]
 
-assign RGB_max = (red>green && red>blue) ? red :
-				 (green>blue) ? green : blue;
-assign RGB_min = (red<green && red<blue) ? red :
-				 (green<blue) ? green : blue;
-assign RGB_diff = ((RGB_max-RGB_min) != 0) ? RGB_max-RGB_min : 8'b1;
-assign Val = RGB_max;
-assign Sat = (Val==0) ? 0 : (100*RGB_diff) / RGB_max;
-assign Hue = (RGB_max==red) ? (60*(green-blue)/RGB_diff) % 360 :
-			 (RGB_max==green) ? (120+(60*(blue-red)/RGB_diff)) % 360 : 
-			 (240+(60*(red-green)/RGB_diff)) % 360;
+wire signed [15:0] conv_x;
+wire signed [15:0] edge_x;
 
-assign red_processed = (Hue>100 && Hue<155) ? red : grey;
-assign green_processed = (Hue>100 && Hue<155) ? green : grey;
-assign blue_processed = (Hue>100 && Hue<155) ? blue : grey;
+reg [7:0] edge_buff [2:0];
 
+integer i;
+always@(posedge clk) begin
+	for (i=2; i>0; i=i-1) begin   //delay line
+		edge_buff[i] <= edge_buff[i-1];
+	end
+	edge_buff[0] <= grey;
+end
 
-// assign red_processed = (RGB_max==red) ? red : 8'h0;
-// assign green_processed = (RGB_max==green) ? green : 8'h0;
-// assign blue_processed = (RGB_max==blue) ? blue : 8'h0;
+assign conv_x = sobel_x[0]*edge_buff[0] + sobel_x[1]*edge_buff[1] + sobel_x[2]*edge_buff[2];
+
+always_comb begin
+	if (conv_x[15] == 1) edge_x = -conv_x;
+	else edge_x = conv_x;
+end
 
 
 
-
-
-
-
-// reg [7:0] r_buff_0, g_buff_0, b_buff_0 [3:0]; //4 pixels
-// reg [7:0] r_buff_1, g_buff_1, b_buff_1 [3:0];
-// reg [7:0] r_buff_2, g_buff_2, b_buff_2 [3:0];
-
-// reg [7:0] horiz_r_buff_0, horiz_g_buff_0, horiz_b_buff_0 [1920:0]; //3 rows + 1 pixel
-// reg [7:0] horiz_r_buff_1, horiz_g_buff_1, horiz_b_buff_1 [1920:0];
-// reg [7:0] horiz_r_buff_2, horiz_g_buff_2, horiz_b_buff_2 [1920:0];
-
-// wire [9:0] r_blur_x_0, g_blur_x_0, b_blur_x_0;   //FF * 4
-// wire [9:0] r_blur_x_1, g_blur_x_1, b_blur_x_1;
-// wire [9:0] r_blur_x_2, g_blur_x_2, b_blur_x_2;
-
-// wire [9:0] r_blur_y_0, g_blur_y_0, b_blur_y_0;   //FF * 4
-// wire [9:0] r_blur_y_1, g_blur_y_1, b_blur_y_1;
-// wire [9:0] r_blur_y_2, g_blur_y_2, b_blur_y_2;
-
-// //shift register
-// integer i;
-// always@(posedge clk) begin
-// 	if (in_valid) begin
-// 		for (i=3; i>0; i=i-1) begin   //delay line
-// 				r_buff_0[i] <= r_buff_0[i-1];
-// 				g_buff_0[i] <= g_buff_0[i-1];	//first buffer
-// 				b_buff_0[i] <= b_buff_0[i-1];
-
-// 				r_buff_1[i] <= r_buff_1[i-1];
-// 				g_buff_1[i] <= g_buff_1[i-1];	//second buffer
-// 				b_buff_1[i] <= b_buff_1[i-1];
-
-// 				r_buff_2[i] <= r_buff_2[i-1];
-// 				g_buff_2[i] <= g_buff_2[i-1];	//third buffer
-// 				b_buff_2[i] <= b_buff_2[i-1];
-// 		end
-// 		r_buff_0[0] <= red;  //store new pixel
-// 		g_buff_0[0] <= green;
-// 		b_buff_0[0] <= blue;
-
-// 		r_buff_1[0] <= r_blur_x_0;	//feed in from prev buffer
-// 		g_buff_1[0] <= g_blur_x_0;
-// 		b_buff_1[0] <= b_blur_x_0;
-
-// 		r_buff_2[0] <= r_blur_x_1;
-// 		g_buff_2[0] <= g_blur_x_1;
-// 		b_buff_2[0] <= b_blur_x_1;
-// 	end
-// end
-
-// //horiz shift register
-// always@(posedge clk) begin
-// 	if (in_valid) begin
-// 		for (i=1920; i>0; i=i-1) begin   //horizontal delay line
-// 				horiz_r_buff_0[i] <= horiz_r_buff_0[i-1];
-// 				horiz_g_buff_0[i] <= horiz_g_buff_0[i-1];	//first horiz buffer
-// 				horiz_b_buff_0[i] <= horiz_b_buff_0[i-1];
-				
-// 				horiz_r_buff_1[i] <= horiz_r_buff_1[i-1];
-// 				horiz_g_buff_1[i] <= horiz_g_buff_1[i-1];	//second horiz buffer
-// 				horiz_b_buff_1[i] <= horiz_b_buff_1[i-1];
-				
-// 				horiz_r_buff_2[i] <= horiz_r_buff_2[i-1];
-// 				horiz_g_buff_2[i] <= horiz_g_buff_2[i-1];	//third horiz buffer
-// 				horiz_b_buff_2[i] <= horiz_b_buff_2[i-1];
-// 		end
-// 		horiz_r_buff_0[0] <= r_blur_x_2;	//feed in from last small buffer
-// 		horiz_g_buff_0[0] <= g_blur_x_2;
-// 		horiz_b_buff_0[0] <= b_blur_x_2;
-		
-// 		horiz_r_buff_1[0] <= r_blur_y_0;
-// 		horiz_g_buff_1[0] <= g_blur_y_0;
-// 		horiz_b_buff_1[0] <= b_blur_y_0;
-		
-// 		horiz_r_buff_2[0] <= r_blur_y_1;
-// 		horiz_g_buff_2[0] <= g_blur_y_1;
-// 		horiz_b_buff_2[0] <= b_blur_y_1;
-// 	end
-// end
-
-// assign r_blur_x_0 = (r_buff_0[0] + r_buff_0[1] + r_buff_0[2] + r_buff_0[3]) >> 2;
-// assign g_blur_x_0 = (g_buff_0[0] + g_buff_0[1] + g_buff_0[2] + g_buff_0[3]) >> 2;
-// assign b_blur_x_0 = (b_buff_0[0] + b_buff_0[1] + b_buff_0[2] + b_buff_0[3]) >> 2;
-
-// assign r_blur_x_1 = (r_buff_1[0] + r_buff_1[1] + r_buff_1[2] + r_buff_1[3]) >> 2;
-// assign g_blur_x_1 = (g_buff_1[0] + g_buff_1[1] + g_buff_1[2] + g_buff_1[3]) >> 2;
-// assign b_blur_x_1 = (b_buff_1[0] + b_buff_1[1] + b_buff_1[2] + b_buff_1[3]) >> 2;
-
-// assign r_blur_x_2 = (r_buff_2[0] + r_buff_2[1] + r_buff_2[2] + r_buff_2[3]) >> 2;
-// assign g_blur_x_2 = (g_buff_2[0] + g_buff_2[1] + g_buff_2[2] + g_buff_2[3]) >> 2;
-// assign b_blur_x_2 = (b_buff_2[0] + b_buff_2[1] + b_buff_2[2] + b_buff_2[3]) >> 2;
-
-// assign r_blur_y_0 = (horiz_r_buff_0[0] + horiz_r_buff_0[640] + horiz_r_buff_0[1280] + horiz_r_buff_0[1920]) >> 2;
-// assign g_blur_y_0 = (horiz_g_buff_0[0] + horiz_g_buff_0[640] + horiz_g_buff_0[1280] + horiz_g_buff_0[1920]) >> 2;
-// assign b_blur_y_0 = (horiz_b_buff_0[0] + horiz_b_buff_0[640] + horiz_b_buff_0[1280] + horiz_b_buff_0[1920]) >> 2;
-
-// assign r_blur_y_1 = (horiz_r_buff_1[0] + horiz_r_buff_1[640] + horiz_r_buff_1[1280] + horiz_r_buff_1[1920]) >> 2;
-// assign g_blur_y_1 = (horiz_g_buff_1[0] + horiz_g_buff_1[640] + horiz_g_buff_1[1280] + horiz_g_buff_1[1920]) >> 2;
-// assign b_blur_y_1 = (horiz_b_buff_1[0] + horiz_b_buff_1[640] + horiz_b_buff_1[1280] + horiz_b_buff_1[1920]) >> 2;
-
-// assign r_blur_y_2 = (horiz_r_buff_2[0] + horiz_r_buff_2[640] + horiz_r_buff_2[1280] + horiz_r_buff_2[1920]) >> 2;
-// assign g_blur_y_2 = (horiz_g_buff_2[0] + horiz_g_buff_2[640] + horiz_g_buff_2[1280] + horiz_g_buff_2[1920]) >> 2;
-// assign b_blur_y_2 = (horiz_b_buff_2[0] + horiz_b_buff_2[640] + horiz_b_buff_2[1280] + horiz_b_buff_2[1920]) >> 2;
-
-// assign red_processed = r_blur_y_2[7:0]; //first 8 bits
-// assign green_processed = g_blur_y_2[7:0];
-// assign blue_processed = b_blur_y_2[7:0];
+assign red_processed = edge_x[7:0]; //first 8 bits
+assign green_processed = edge_x[7:0];
+assign blue_processed = edge_x[7:0];
 
 endmodule
